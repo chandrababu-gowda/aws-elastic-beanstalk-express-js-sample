d6f03a00218278d6d2ef14939e44956093149356
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2024-11-03 10:57:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello Thejaswini Chandrababu Gowda"));
+app.get("/", (req, res) => res.send("Hello TheCha"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
